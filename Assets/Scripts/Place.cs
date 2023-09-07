72a77c0b8643fea1967c7ede28a848d3c913e59b
Arstotzk <50625444+Arstotzk@users.noreply.github.com>
2023-09-07 20:08:49 +0700

    Подсветка мест куда можно положить карту

--- Assets/Scripts/Place.cs
+++ Assets/Scripts/Place.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Place : MonoBehaviour
 {
@@ -8,9 +9,10 @@ public class Place : MonoBehaviour
     public ParticleSystem particleSystem;
     public int column;
     public int row;
+    public DeployManager deployManager;
     void Start()
-    { 
-
+    {
+        deployManager = GameObject.Find("DeployManager").GetComponent<DeployManager>();
     }
 
     public void StartAttackShow()
@@ -20,5 +22,25 @@ public class Place : MonoBehaviour
     public void StopAttackShow()
     {
         particleSystem.Stop();
+    }
+
+    public void OnMouseEnter()
+    {
+         //if (deployManager.isPlayerDrugCard)
+            SetCollorSelect();
+    }
+    public void OnMouseExit()
+    {
+        //if (deployManager.isPlayerDrugCard)
+            SetCollorUnselect();
+    }
+
+    public void SetCollorSelect()
+    {
+        GetComponent<Image>().color = new Color32(255, 255, 225, 100);
+    }
+    public void SetCollorUnselect()
+    {
+        GetComponent<Image>().color = new Color32(255, 255, 225, 0);
     }
 }
